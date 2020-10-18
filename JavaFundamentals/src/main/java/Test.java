d8a1903a06d7bea87766299a5c97257b9c2702de
Bogdan Morar <bogdanmorar5@gmail.com>
2020-10-18 12:18:35 +0300

    add arrayList

--- JavaFundamentals/src/main/java/Test.java
+++ JavaFundamentals/src/main/java/Test.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class Test {
     public static void main(String[] args) {
 
@@ -5,6 +7,17 @@ public class Test {
         String testName = "helloWorld";
             System.out.println(testName);
 
+        ArrayList<String> stringArrayList = new ArrayList<>();
+        stringArrayList.add("1");
+        stringArrayList.add("2");
+        stringArrayList.add("3");
+        stringArrayList.add("4");
+        stringArrayList.add("5");
+        stringArrayList.add("6");
+
+        System.out.println(stringArrayList);
+
+        System.out.println("HelloDarkestMyOldFriend");
 
     }
 }
